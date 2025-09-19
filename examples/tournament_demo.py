bb982aaa0f770b0919896be667027b3a6cc46f1c
eak <eak@ivalua.com>
2025-09-19 18:17:47 +0000

    Remove unused random import from tournament_demo.py

--- examples/tournament_demo.py
+++ examples/tournament_demo.py
@@ -14,7 +14,6 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from config import TournamentConfig
 from tournament import LudoTournament
-import random
 
 
 def main():
